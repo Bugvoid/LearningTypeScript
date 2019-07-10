c299e404abe3b9009c90738c3090ba2c2a93f3a4
Luan Alves <luan.alves@comunike.me>
2019-07-10 09:13:23 -0300

    first commit

---
+++ Basic/app.ts
@@ -0,0 +1,12 @@
+class produto {
+  nome: string;
+  preco: string;
+  constructor(_nome: string, _preco: string) {
+    this.nome;
+    this.preco;
+  }
+
+  getDados() {
+    return "Nome do produto: " + this.nome + "preco: " + this.preco;
+  }
+}
